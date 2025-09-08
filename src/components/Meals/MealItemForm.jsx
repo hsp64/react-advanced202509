3d6f7a4a940419c4c31f9c3d341467af90ed6842
hsp64 <pphs604@gmail.com>
2025-09-08 10:54:05 +0900

    ref 전달로 수량 끌어올리기

--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -1,23 +1,33 @@
 import Input from '../UI/Input';
 import styles from './MealItemForm.module.scss';
+import {useRef} from "react";
 
-const MealItemForm = (props) => {
-  return (
-    <form className={styles.form}>
-      <Input
-        label='수량'
-        inputAttr={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1'
-        }}
-      />
-      <button>담기</button>
-    </form>
-  );
+const MealItemForm = ({id, onAddToCart}) => {
+
+    const inputRef = useRef();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        onAddToCart(inputRef.current.value);
+    };
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <Input
+                ref={inputRef}
+                label='수량'
+                inputAttr={{
+                    id: 'amount_' + id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1'
+                }}
+            />
+            <button>담기</button>
+        </form>
+    );
 };
 
 export default MealItemForm;
