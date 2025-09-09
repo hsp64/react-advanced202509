e7719521fa963a304a729c8bafd7065acb940505
hsp64 <pphs604@gmail.com>
2025-09-09 11:39:26 +0900

    persist middleware로 새로고침시에도 로그인 상태 유지

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ import ZustandCounter from './zustand-practice/components/ZustandCounter.jsx';
 import {useCounterStore} from "./zustand-practice/store/counterStore.js";
 import Header from "./zustand-practice/components/Header.jsx";
 import Auth from "./zustand-practice/components/Auth.jsx";
+import UserProfile from "./zustand-practice/components/UserProfile.jsx";
+import {useAuthStore} from "./zustand-practice/store/authStore.js";
 
 const App = () => {
+
+    const {isLoggedIn} =useAuthStore();
 
     const {count} = useCounterStore();
 
     return (
         <>
             <Header />
-            <Auth />
+            {isLoggedIn ? <UserProfile /> : <Auth />}
             <ZustandCounter />
         </>
     );
